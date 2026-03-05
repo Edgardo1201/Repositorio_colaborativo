0b9107e64a4737bc4e39fb36b7d95d39574b6237
Edgardo <edgardo.miranda@uip.edu.pa>
2026-03-05 16:27:40 -0500

    Agregando el main y modulo conversiones

---
+++ conversiones.py
@@ -0,0 +1,17 @@
+def celsius_a_fahrenheit(c):
+    return (c * 9/5) + 32
+
+def fahrenheit_a_celsius(f):
+    return (f - 32) * 5/9
+
+def kilometros_a_millas(km):
+    return km * 0.621371
+
+def millas_a_kilometros(mi):
+    return mi / 0.621371
+
+def kilos_a_libras(kg):
+    return kg * 2.20462
+
+def libras_a_kilos(lb):
+    return lb / 2.20462
---
+++ main.py
@@ -0,0 +1,10 @@
+from conversiones import *
+
+print("\n=== CONVERSION DE UNIDADES ===\n")
+
+km = 130
+kg = 76
+c = 250
+print(f"{km} Kilometros son {kilometros_a_millas(km):.2f} millas")
+print(f"{kg} kilos son {kilos_a_libras(kg):.2f} libras")
+print(f"{c} celsius son {celsius_a_fahrenheit(c):.2f} fahrenheit")
