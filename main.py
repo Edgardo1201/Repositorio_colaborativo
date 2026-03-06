fb0c40a336bb1727be8abb079f49d68be35dcfed
edmcruz <edmanuel.cruz@uip.edu.pa>
2026-03-06 09:58:55 -0500

    Actualizando main.py

--- main.py
+++ main.py
@@ -1,10 +1,33 @@
 from conversiones import *
+from cuento import mostrar_cuento
+from operaciones import *
 
-print("\n=== CONVERSION DE UNIDADES ===\n")
+def main():
+    print("=== MENÚ PRINCIPAL ===")
+    print("1. Operaciones matemáticas")
+    print("2. Conversiones")
+    print("3. Mostrar cuento")
 
-km = 130
-kg = 76
-c = 250
-print(f"{km} Kilometros son {kilometros_a_millas(km):.2f} millas")
-print(f"{kg} kilos son {kilos_a_libras(kg):.2f} libras")
-print(f"{c} celsius son {celsius_a_fahrenheit(c):.2f} fahrenheit")
+    opcion = input("Seleccione una opción: ")
+
+    if opcion == "1":
+        a = float(input("Ingrese el primer número: "))
+        b = float(input("Ingrese el segundo número: "))
+
+        print("Suma:", sumar(a, b))
+        print("Resta:", restar(a, b))
+        print("Multiplicación:", multiplicar(a, b))
+        print("División:", dividir(a, b))
+
+    elif opcion == "2":
+        km = float(input("Ingrese kilómetros: "))
+        print(f"{km} kilómetros son {kilometros_a_millas(km):.2f} millas")
+
+    elif opcion == "3":
+        mostrar_cuento()
+
+    else:
+        print("Opción no válida")
+
+if __name__ == "__main__":
+    main()
